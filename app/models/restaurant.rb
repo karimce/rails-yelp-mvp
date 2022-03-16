3970b01fbaa2f3089992b89432cb171ecba78d56
karim chlih <linasanet@gmail.com>
2022-03-16 15:02:01 +0100

    Rails-yelp-mvp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = %w[chinese italian japanese french belgian]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  has_many :reviews, dependent: :destroy
+
+end
